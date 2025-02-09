89a362b7dbe4cf360248df49ccac222a65b76bd9
OMID <omidhacker200@gmail.com>
2025-02-09 14:02:36 +0330

    First commit with project files

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ def index():
     return render_template('index.html',BTC=BTC,TRX=TRX,TON=TON,tether=tether,GOLD=GOLD)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
